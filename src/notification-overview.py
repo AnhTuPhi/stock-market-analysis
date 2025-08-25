854018eaa397a6aae588299ea451959b25613a9d
TuPhiAnh <phianhtu2211@gmail.com>
2025-08-25 22:59:44 +0700

    update message

--- src/notification-overview.py
+++ src/notification-overview.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     logger.info('Start notifying')
 
     with Telegram() as tele:
-        text = "🔥 Thông tin overview đã được tổng hợp tại: https://github.com/AnhTuPhi/stock-market-analysis/tree/master/src/data/stock"
+        text = "🔥 Thông tin overview đã được tổng hợp tại: https://github.com/AnhTuPhi/stock-market-analysis/tree/master/data/stock"
         tele.send_message(text, parse_mode=None)
